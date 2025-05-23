df0bcffaf908c5d897f34ca68ce91801ea2c1f84
Danica <danicacm@live.com>
2025-05-23 17:07:51 -0400

    Revised __init__ to work with frontend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -17,7 +17,10 @@ def create_app(config_name=None):
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "test-jwt-secret")
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "test-secret")
 
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    CORS(app, supports_credentials=True, origins=[
+        "http://localhost:5173",
+        "https://sun-kissed-and-southern.netlify.app"
+    ])
 
     db.init_app(app)
     migrate.init_app(app, db)
